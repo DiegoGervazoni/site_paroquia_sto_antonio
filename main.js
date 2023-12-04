f6d0c6a78dd70a6eed4cf705a5d721db5a2ba03c
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-12-04 19:09:52 -0300

    atualizando alerts

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
 const form = document.getElementById("contact-form"); //Pegando o formulário
+const nome = document.getElementById("nome"); //Pegando o nome
+let formValido = false; //Variável para validar o formulário
 
 function validaNome(nomeCompleto) {
   //Função para validar o nome
   const nome = nomeCompleto.split(" ");
-  return nome.length >= 3;
+  return nome.length >= 2;
 }
 
 form.addEventListener("submit", function (e) {
   //Função para enviar o formulário
   e.preventDefault(); //Função para parar o envio do formulário
 
-  let formValido = false; //Variável para validar o formulário
-
-  const nome = document.getElementById("nome"); //Pegando o nome
   const email = document.getElementById("email"); //Pegando o email
   const telefone = document.getElementById("tel"); //Pegando o telefone
   const mensagem = document.getElementById("message"); //Pegando a mensagem
-  const mensagemSucesso = `Obrigado ${nome.value}, pela sua mensagem! Em breve entraremos em contato com você através do seu e-mail ${email.value}`; //Pegando a mensagem de sucesso
+  const mensagemSucesso = `Obrigado pela sua mensagem, ${nome.value}.`; //Pegando a mensagem de sucesso
 
   formValido = validaNome(nome.value); //Validando o formulário
-
   if (formValido) {
-    //Validando o nome
-    alert(mensagemSucesso);
-    nome.value = " ";
-    email.value = " ";
-    telefone.value = " ";
-    mensagem.value = " ";
+    const containerSucessoMensagem = document.querySelector("#sucess-message");
+    containerSucessoMensagem.textContent = mensagemSucesso;
+    containerSucessoMensagem.style.display = "block";
+
+    nome.value = "";
+    email.value = "";
+    telefone.value = "";
+    mensagem.value = "";
   } else {
-    //Se o nome for inválido
-    alert("Por favor, informe seu nome completo");
+    nomeBeneficiario.style.border = "1px solid red";
+    document.querySelector(".error-message").style.display = "block";
+  }
+});
+
+nome.addEventListener("keyup", function (e) {
+  e.preventDefault();
+  console.log(e.target.value);
+  formValido = validaNome(e.target.value);
+
+  if (!formValido) {
+    nome.classList.add("error");
+    document.querySelector(".error-message").style.display = "block";
+  } else {
+    nome.classList.remove("error");
+    document.querySelector(".error-message").style.display = "none";
   }
 });
