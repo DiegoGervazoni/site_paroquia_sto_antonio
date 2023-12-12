512163cdca3c86f609d0f019fa9d1af38699371c
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-12-12 14:49:31 -0300

    alteracao de id telefone no html, alteracao de cor no css (vermelho para azul), e validacao de form no jquery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,48 @@
 $(document).ready(function () {
+  $("#telefone").mask("(00) 00000-0000");
 
   $(".menu-hamburguer").click(function () {
     //quando clicar no menu hamburguer
     $("nav").slideToggle(); //abre e fecha o menu
   });
-});
 
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+        minlength: 3,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+      message: {
+        required: true,
+        minlength: 5,
+      },
+    },
+    messages: {
+      nome: '<span class="campoErrado">Por favor, insira o seu nome.</span>',
+      email: '<span class="campoErrado">Por favor, insira o seu email.</span>',
+      telefone:
+        '<span class="campoErrado">Por favor, insira o seu telefone.</span>',
+      message:
+        '<span class="campoErrado">Por favor, insira a sua mensagem.</span>',
+    },
+    submitHandler: function (form) {
+      form.submit();
+      alert("Mensagem enviada com sucesso!");
+    },
+    invalidHandler: function (evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if (camposIncorretos) {
+        alert(
+          `Existem ${camposIncorretos} campos incorretos. Por favor, verifique os campos e tente novamente.`
+        );
+      }
+    },
+  });
+});
