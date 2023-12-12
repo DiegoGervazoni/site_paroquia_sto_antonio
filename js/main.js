6ae5a5b441a4426ef99ab68592f534d508e4fee3
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-12-12 11:58:39 -0300

    removendo carrosel do html, css e Jquery, removendo slick-theme do head

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function () {
 
-  $("#carrosel-imagens").slick({
-    //seleciona o elemento que vai ser o carrosel
-    autoplay: true, //autoplay
-  });
-
   $(".menu-hamburguer").click(function () {
     //quando clicar no menu hamburguer
     $("nav").slideToggle(); //abre e fecha o menu
   });
-
 });
+
