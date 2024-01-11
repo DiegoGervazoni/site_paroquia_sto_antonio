8c1ee07727d690ef2f453552239cd7234db8e9e8
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-01-11 10:40:05 -0300

    Refatorando todo o projeto, implementando Bootrasp, nova responsividade no CSS e adicionado JQuery com atualizacoes

--- js/main.js
+++ js/main.js
@@ -1,44 +1,28 @@
 $(document).ready(function () {
   $("#telefone").mask("(00) 00000-0000");
 
-  $(".menu-hamburguer").click(function () {
-    //quando clicar no menu hamburguer
-    $("nav").slideToggle(); //abre e fecha o menu
-  });
-
-  $(".botao-menu button").click(function () {
-    $(".menu").slideToggle();
-  });
-
   $("form").validate({
     rules: {
       nome: {
         required: true,
-        minlength: 3,
       },
       email: {
         required: true,
         email: true,
       },
-      telefone: {
+      mensagem: {
         required: true,
-      },
-      message: {
-        required: true,
-        minlength: 5,
       },
     },
     messages: {
       nome: '<span class="campoErrado">Por favor, insira o seu nome.</span>',
       email: '<span class="campoErrado">Por favor, insira o seu email.</span>',
-      telefone:
-        '<span class="campoErrado">Por favor, insira o seu telefone.</span>',
-      message:
+      mensagem:
         '<span class="campoErrado">Por favor, insira a sua mensagem.</span>',
     },
     submitHandler: function (form) {
       form.submit();
-      alert("Mensagem enviada com sucesso!");
+      alert("Formulário enviado com sucesso!");
     },
     invalidHandler: function (evento, validador) {
       let camposIncorretos = validador.numberOfInvalids();
